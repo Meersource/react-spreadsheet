0b43d5a816f2b366a3214139fcda4f94a8364a90
Hamza <Mmalikhamza44@gmail.com>
2021-09-24 11:03:00 +0500

    row select update 23/sep/2021

--- src/Sheet.js
+++ src/Sheet.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import "./App.css";
+import './App.css'
 import Spreadsheet from "react-spreadsheet";
 
-export default function App() {
+export default function Sheet() {
   const [data, setData] = useState([
     [{ value: 45 }, { value: 13 }, { value: 23 }],
     [{ value: 34 }, { value: 53 }, { value: 55 }],
@@ -16,30 +16,37 @@ export default function App() {
   const [MSCP, setMSCP] = useState([]);
   const [MSCPCopyData, setMSCPCopyData] = useState([]);
 
-  const [singleCopy, setSingleCopy] = useState();
-  const [sLineCopy, setSLineCopy] = useState([]);
-  // const [mulState, setMulState] = useState();
+  const [singleCopy, setSingleCopy] = useState(); // State for copied Value
+  const [mulState,setMulState]=useState()
 
+  // const ourMSCPHandler = (fData) => {
+  //   setMSCP(fData);
+  // };
   const onSelectCall = (fData) => {
-    if (fData.length > 1) {
-      let newArr = [];
+    // Called When onSelect of spreadsheet triggers
+    // const fData= data
+    if(fData.length > 1) {
+      let newArr =[]
       console.log("Multiple Selection", fData.length);
       for (let i = 0; i < fData.length; i++) {
-        const Coordi = {
-          row: fData[i].row,
-          column: fData[i].column,
-        };
-        const nb = data[Coordi.row][Coordi.column];
-        console.log("valueBySelect", nb);
-        newArr.push(Coordi);
+        
+        const Coordi={
+           row : fData[i].row,
+           column : fData[i].column
+        }
+         const nb=data[Coordi.row][Coordi.column]
+      console.log("valueBySelect",nb);
+
+        newArr.push(Coordi)
         // ourMSCPHandler(fData);
-        setMSCP(newArr);
+        setMSCP(newArr)
         // console.log("arr",newArr);
+        // console.log("v",data[MSCP.row][MSCP.row].value);
         // console.log("valuez",data[MSCP.newArr.row][MSCP.newArr.column].value);
-        console.log("selected 0:", data[Coordi.row][Coordi.column].value);
+        console.log("selected 0:", data[Coordi.row][Coordi.column].value)
         // console.log("selectedState",data[MSCP.row][MSCP.column].value);
       }
-      console.log("cd", MSCP);
+      console.log("cd",MSCP);
     } else {
       const points = {
         // Destructuring data into positon for position points
@@ -60,6 +67,13 @@ export default function App() {
     );
   };
 
+  const onCommitCall = (data) => {}; // Called When onCellCommit of spreadsheet triggers
+
+  const onActivateCall = (data) => {
+    // Called When onActivate of spreadsheet triggers
+    // console.log("On Activate Call", data);;;;;
+  };
+
   const copyDataHandler = () => {
     // Called When the Copy button is clicked
     const ourData = data;
@@ -73,12 +87,47 @@ export default function App() {
   };
 
   const copyMulDataHandler = () => {
-    const selecteValues = MSCP.map((i) => data[i.row][i.column].value);
-    console.log("sl", selecteValues);
+    // Called When the Copy button is clicked
+    // for(let i=0; i<MSCP.length; i++){
+      const ourData = data;
+      
+      const row = MSCP.map((i)=>{
+        return i.row
+      });
+      // console.log("rr",row);
+      const column = MSCP.map((i)=>{
+        return i.column
+      });
+      console.log("copMul",row);
+      const copiedValue = ourData[row][column].value;
+      console.log("bbb",copiedValue);
+      // setMulState(copiedValue)
+      // console.log(mulState);
+      console.log("copy", ourData[row][column].value);
+    // }
+    // setSingleCopy(copiedValue); // set state of copied value to singleCopy
 
-    setSLineCopy(selecteValues);
-    console.log("1q", sLineCopy);
+    // console.log("copy", ourData[row][column].value);
   };
+
+  // const MultiCopyDataHandler = () => {
+  //   // Called When the Copy button is clicked
+  //   MSCP.forEach((e)=>{
+  //     const row= e.row
+  //     const column= e.column
+  //     const newData = []
+  //     newData.push(data[row][column].value)
+  //     setMSCPCopyData(newData);
+  //   })    ;;;;;;;
+    // const row = MSCP.row;
+    // const column = MSCP.column;
+    // const copiedValue = ourData[row][column].value;
+
+    // setSingleCopy(copiedValue); // set state of copied value to singleCopy
+
+    // console.log("copy", ourData[row][column].value);
+  // };;;;
+
   const pasteDataHandler = () => {
     // Called when the Paste button is clicked
     setData(
@@ -100,6 +149,7 @@ export default function App() {
     onSelectCall(selectedCellPoint);
     onChangeCall(data);
   };
+  
 
   const consoleHanler = () => {
     console.log("MSCP: ", MSCP);
@@ -107,25 +157,19 @@ export default function App() {
   };
   return (
     <div className="App">
-      <h1>React-SpreadSheet{sLineCopy}</h1>
-    {console.log("1q", sLineCopy)}
-
+      <h1>React-SpreadSheet</h1>
       <div className="section-design">
-        <button onClick={copyDataHandler} className="btn-design">
-          Copy
-        </button>
-        <button onClick={pasteDataHandler} className="btn-design">
-          Paste
-        </button>
-        <button onClick={cutDataHandler} className="btn-design">
-          Cut
-        </button>
-        <button onClick={consoleHanler}>Console</button>
-        <button onClick={copyMulDataHandler}>Mul Copy</button>
-        {/* <button onClick={MultiCopyDataHandler}>Multi</button> */}
+      <button onClick={copyDataHandler} className='btn-design'>Copy</button>
+      <button onClick={pasteDataHandler}  className='btn-design'>Paste</button>
+      <button onClick={cutDataHandler} className='btn-design'>Cut</button>
+      <button onClick={consoleHanler}>Console</button>
+      <button onClick={copyMulDataHandler}>Mul Copy</button>
+      {/* <button onClick={MultiCopyDataHandler}>Multi</button> */}
       </div>
       <Spreadsheet
         data={data}
+        onCellCommit={(data) => onCommitCall(data)}
+        onActivate={(data) => onActivateCall(data)}
         onChange={(data) => onChangeCall(data)}
         onSelect={(data) => {
           if (data.length === 1) {
@@ -137,6 +181,11 @@ export default function App() {
         columnLabels={["Gas0", "Power1", "Emissions2"]}
         rowLabels={["Q0", "Q1", "Q2", "Q3"]}
       />
+      {/* <button onClick={copyDataHandler}>Copy</button>
+      <button onClick={pasteDataHandler}>Paste</button>
+      <button onClick={cutDataHandler}>Cut</button>
+      <button onClick={consoleHanler}>Console</button>
+      <button onClick={MultiCopyDataHandler}>Multi</button> */}
     </div>
   );
 }
