a4780368ed4270db6e74cbf15c2be2cbd6a1f617
Hamza <Mmalikhamza44@gmail.com>
2021-09-27 17:03:20 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ export default function App() {
     [{ value: 45 }, { value: 90 }, { value: 0.33 }],
     [{ value: 78 }, { value: 0.13 }, { value: 54 }],
   ]); // State for Data of the Spreadsheet
-  const [selectedCellPoint, setSelectedCellPoint] = useState({}); // State for Selected Cell Points
+  // const [selectedCellPoint, setSelectedCellPoint] = useState({}); // State for Selected Cell Points
   const [MSCP, setMSCP] = useState([]);
 
-  const [singleCopy, setSingleCopy] = useState();
+  // const [singleCopy, setSingleCopy] = useState();
   const [sLineCopy, setSLineCopy] = useState([]);
-  // const [mulState, setMulState] = useState();
 
   const onSelectCall = (fData) => {
-    console.log(fData)
+    console.log(fData);
     if (fData) {
       let newArr = [];
       for (let i = 0; i < fData.length; i++) {
@@ -28,85 +27,125 @@ export default function App() {
         newArr.push(Coordi);
         setMSCP(newArr);
       }
-    } else {
-      const points = {
-        // Destructuring data into positon for position points
-        row: fData.row,
-        column: fData.column,
-      };
-      setSelectedCellPoint(points); //Called to set Selected Cell Points of the cell
     }
+    // else {
+    //   const points = {
+    //     // Destructuring data into positon for position points
+    //     row: fData.row,
+    //     column: fData.column,
+    //   };
+    //   setSelectedCellPoint(points); //Called to set Selected Cell Points of the cell
+    // }
   };
 
+  // const copyDataHandler = () => {
+  //   // Called When the Copy button is clicked
+  //   const ourData = data;
+  //   const row = selectedCellPoint.row;
+  //   const column = selectedCellPoint.column;
+  //   const copiedValue = ourData[row][column].value;
 
-  const copyDataHandler = () => {
-    // Called When the Copy button is clicked
-    const ourData = data;
-    const row = selectedCellPoint.row;
-    const column = selectedCellPoint.column;
-    const copiedValue = ourData[row][column].value;
+  //   setSingleCopy(copiedValue); // set state of copied value to singleCopy
+  //   console.log("copy", ourData[row][column].value);
 
-    setSingleCopy(copiedValue); // set state of copied value to singleCopy
-
-  };
+  // };
 
   const copyMulDataHandler = () => {
     const selecteValues = MSCP.map((i) => data[i.row][i.column].value);
     setSLineCopy(selecteValues);
   };
   const pasteMulDataHandler = () => {
-    let sData = [...data]
-    MSCP.forEach(function (number, index) {
-      sData[number.row][number.column].value = sLineCopy[index]
-    });
-    setData(sData)
-  }
+    if(sLineCopy.length >= 1) {
 
-  const pasteDataHandler = () => {
-
-    data[selectedCellPoint.row][selectedCellPoint.column].value = singleCopy
-
-    onSelectCall(selectedCellPoint); // Called When onSelect of spreadsheet triggers
-    setData(data);
+      let sData = [...data];
+      MSCP.forEach(function (number, index) {
+        sData[number.row][number.column].value = sLineCopy[index];
+        console.log("Number: ", number);
+        console.log("Index: ", index);
+        if(MSCP.length < sLineCopy.length ){
+          console.log("sData.length", MSCP.length);
+          console.log("sLineCopy.length", sLineCopy.length);
+          sData.push([{ value: "" }, { value: "" }, { value: "" }]);
+          setData(sData);
+    
+        }
+      });
+  
+      // if(sData.length < sLineCopy.length ){
+      //   sData.push([{ value: "" }, { value: "" }, { value: "" }]);
+  
+      // }
+      setData(sData);
+    }
   };
 
+  // const pasteDataHandler = () => {
+
+  //   data[selectedCellPoint.row][selectedCellPoint.column].value = singleCopy
+
+  //   onSelectCall(selectedCellPoint); // Called When onSelect of spreadsheet triggers
+  //   setData(data);
+  // };
   const cutDataHandler = () => {
-    copyDataHandler();
-    data[selectedCellPoint.row][selectedCellPoint.column].value = "";
-    onSelectCall(selectedCellPoint);
-    setData(data);
+    copyMulDataHandler();
+    let sData = [...data];
+    MSCP.forEach(function (number, index) {
+      sData[number.row][number.column].value = "";
+    });
+    setData(sData);
   };
 
-  console.log()
+  // const cutDataHandler = () => {
+  //   copyDataHandler();
+  //   data[selectedCellPoint.row][selectedCellPoint.column].value = "";
+  //   onSelectCall(selectedCellPoint);
+  //   setData(data);
+  // };
+  const addRowHandler = () => {
+    let nData = [...data];
+    nData.push([{ value: "" }, { value: "" }, { value: "" }]);
+    // for (var i = 0; i < nData.length; i++) {
+    //   nData[i].push({ value: 1 });
+    //   // nData.concat(newData);s
+    // }
+    setData(nData);
+  };
+
+  const addColHandler = () => {
+    let nData = [...data];
+    for (var i = 0; i < nData.length; i++) {
+      nData[i].push({ value: "" });
+    }
+    setData(nData);
+  };
 
   return (
     <div className="App">
       <h1>React-SpreadSheet</h1>
 
       <div className="section-design">
-        <button onClick={copyDataHandler} className="btn-design">
+        <button onClick={copyMulDataHandler} className="btn-design">
           Copy
         </button>
-        <button onClick={pasteDataHandler} className="btn-design">
+        <button onClick={pasteMulDataHandler} className="btn-design">
           Paste
         </button>
         <button onClick={cutDataHandler} className="btn-design">
           Cut
         </button>
-        <button onClick={copyMulDataHandler}>Mul Copy</button>
-        <button onClick={pasteMulDataHandler}>Mul paste</button>
+        <button onClick={addRowHandler} className="btn-design">
+          Add Row
+        </button>
+        <button onClick={addColHandler} className="btn-design">
+          Add Column
+        </button>
       </div>
       <Spreadsheet
         data={data}
         onChange={(data) => setData(data)}
         onSelect={(data) => {
-          if (data.length === 1) {
-            onSelectCall(data[0]);
-          } else {
-            onSelectCall(data);
-          }
+          onSelectCall(data);
         }}
-        columnLabels={["Gas0", "Power1", "Emissions2"]}
         rowLabels={["Q0", "Q1", "Q2", "Q3"]}
       />
     </div>
